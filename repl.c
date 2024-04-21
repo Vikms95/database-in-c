b4f0d517a09bf5c98412b19ad63eea2f039cd672
Vikms95 <vms1995@hotmail.com>
2024-04-21 23:25:10 +0200

    feat(portal): handle negative id or string too long edge cases

--- repl.c
+++ repl.c
@@ -1,3 +1,4 @@
+// PART 5
 // *pointer_address->value = 123 - modify value of where the pointer is pointing to
 // &(pointer_address) -  reference the pointer address in memory, not where the pointer points to
 // pointer_address->value = NULL - usually always used with NULL, since it modifies where the pointer is pointing, the pointer address
@@ -33,6 +34,8 @@ typedef enum
 {
     PREPARE_SUCCESS,
     PREPARE_SYNTAX_ERROR,
+    PREPARE_STRING_TOO_LONG,
+    PREPARE_NEGATIVE_ID,
     PREPARE_UNRECOGNIZED_STATEMENT,
 } PrepareResult;
 
@@ -52,8 +55,9 @@ typedef struct
 typedef struct
 {
     uint32_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    // Allocating an extra character since all C strings should be ended with a NULL character
+    char username[COLUMN_USERNAME_SIZE + 1];
+    char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 
 typedef struct
@@ -148,19 +152,50 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer)
     }
 }
 
+PrepareResult prepare_insert(InputBuffer *input_buffer, Statement *statement)
+{
+    statement->type = STATEMENT_INSERT;
+
+    // Separate buffer on whitespace
+    char *keyword = strtok(input_buffer->buffer, " ");
+    char *id_string = strtok(NULL, " ");
+    char *username = strtok(NULL, " ");
+    char *email = strtok(NULL, " ");
+
+    if (id_string == NULL || username == NULL || email == NULL)
+    {
+        return PREPARE_SYNTAX_ERROR;
+    }
+
+    if (strlen(username) > COLUMN_USERNAME_SIZE)
+    {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if (strlen(email) > COLUMN_EMAIL_SIZE)
+    {
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    int id = atoi(id_string);
+
+    if(id < 0) {
+        return PREPARE_NEGATIVE_ID;
+    }
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS;
+}
+
 PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
 {
     /* tries to match 6 characters of the buffer, since insert will be followed
      by more data afterwards the keyword*/
     if (strncmp(input_buffer->buffer, "insert", 6) == 0)
     {
-        statement->type = STATEMENT_INSERT;
-        int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
-        if (args_assigned < 3)
-        {
-            return PREPARE_SYNTAX_ERROR;
-        }
-        return PREPARE_SUCCESS;
+        return prepare_insert(input_buffer, statement);
     }
     if (strcmp(input_buffer->buffer, "select") == 0)
     {
@@ -303,6 +338,12 @@ int main(int argc, char *argv[])
             break;
         case (PREPARE_SYNTAX_ERROR):
             printf("Syntax error. Could not parse statement.\n");
+            continue;
+        case (PREPARE_STRING_TOO_LONG):
+            printf("String too long.\n");
+            continue;
+        case (PREPARE_NEGATIVE_ID):
+            printf("Row ID is negative, which is not allowed.\n");
             continue;
         case (PREPARE_UNRECOGNIZED_STATEMENT):
             printf("Unrecognized keyword at start of '%s'.\n",
