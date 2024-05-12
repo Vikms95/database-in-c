485cdf1587cffd91f1ee5c310e5daee3ad750fde
Vikms95 <vms1995@hotmail.com>
2024-05-12 18:42:28 +0200

    feat: add variables for lead node format

--- repl.c
+++ repl.c
@@ -51,6 +51,7 @@ typedef enum
     EXECUTE_SUCCESS,
 } ExecuteResult;
 
+// Leaf nodes and internal nodes have different sizes, differentiate them.
 typedef enum
 {
     NODE_INTERNAL,
@@ -123,6 +124,7 @@ const uint32_t PAGE_SIZE = 4096;
 const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
 const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
+// Node header format
 const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t);
 const uint32_t NODE_TYPE_OFFSET = 0;
 const uint32_t IS_ROOT_SIZE = sizeof(uint8_t);
@@ -131,6 +133,10 @@ const uint32_t PARENT_POINTER_SIZE = NODE_TYPE_SIZE;
 const uint32_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
 const uint32_t COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
 
+// Leaf node format
+const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE;
 /*
  * Creates a cursor at the starting point of the table.
  */
