a53dfa7337abbf0868a139a4815ad28a905d1367
Vikms95 <vms1995@hotmail.com>
2024-05-12 20:48:20 +0200

    feat(portal): add print leaf node function

--- repl.c
+++ repl.c
@@ -298,7 +298,6 @@ void db_close(Table *table)
     // Access the pager reference stored within the table struct.
     Pager *pager = table->pager;
     // This only accounts for fully written pages, partially written are handled after the loop
-    uint32_t num_full_pages = table->num_rows / ROWS_PER_PAGE;
 
     // Empty whatever reference the database might have
     for (uint32_t i = 0; i < pager->num_pages; i++)
@@ -464,6 +463,18 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table)
     {
         db_close(table);
         exit(EXIT_SUCCESS);
+    }
+    else if (strcmp(input_buffer->buffer, ".btree") == 0)
+    {
+        printf("Tree:\n");
+        print_leaf_node(get_page(table->pager, 0));
+        return META_COMMAND_SUCCESS;
+    }
+    else if (strcmp(input_buffer->buffer, ".constants") == 0)
+    {
+        printf("Constants:\n");
+        print_constants();
+        return META_COMMAND_SUCCESS;
     }
     else
     {
@@ -650,6 +661,17 @@ void print_constants()
     printf("LEAF_NODE_CELL_SIZE: %d\n", LEAF_NODE_CELL_SIZE);
     printf("LEAF_NODE_SPACE_FOR_CELLS: %d\n", LEAF_NODE_SPACE_FOR_CELLS);
     printf("LEAF_NODE_MAX_CELLS: %d\n", LEAF_NODE_MAX_CELLS);
+}
+
+void print_leaf_node(void *node)
+{
+    uint32_t num_cells = *leaf_node_num_cells(node);
+    printf("leaf (size %d)\n", num_cells);
+    for (uint32_t i = 0; i < num_cells; i++)
+    {
+        uint32_t key = *leaf_node_key(node, i);
+        printf("  - %d : %d\n", i, key);
+    }
 }
 
 /*
